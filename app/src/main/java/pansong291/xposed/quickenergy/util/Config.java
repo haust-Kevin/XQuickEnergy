dd64cb35db4379caf64c0b5ba775742c50f9590c
wangdingbang <3364917990@qq.com>
2023-09-16 15:24:36 +0800

    update

--- app/src/main/java/pansong291/xposed/quickenergy/util/Config.java
+++ app/src/main/java/pansong291/xposed/quickenergy/util/Config.java
@@ -126,6 +126,8 @@ public class Config {
     public static final String jn_stallSelfOpenTime = "tallSelfOpenTime";
     public static final String jn_stallDonate = "stallDonate";
     public static final String jn_stallInviteRegister = "stallInviteRegister";
+    public static final String jn_stallThrowManure = "stallThrowManure";
+    public static final String jn_stallInviteShopList = "stallInviteShopList";
 
     /* other */
     public static final String jn_receivePoint = "receivePoint";
@@ -142,6 +144,7 @@ public class Config {
     public static final String jn_collectSesame = "collectSesame";
     public static final String jn_zcjSignIn = "zcjSignIn";
     public static final String jn_merchantKmdk = "merchantKmdk";
+    public static final String jn_greenFinance = "greenFinance";
 
     public static volatile boolean shouldReload;
     public static volatile boolean hasChanged;
@@ -257,6 +260,8 @@ public class Config {
     private int stallSelfOpenTime;
     private boolean stallDonate;
     private boolean stallInviteRegister;
+    private boolean stallThrowManure;
+    private List<String> stallInviteShopList;
 
     /* other */
     private boolean receivePoint;
@@ -273,6 +278,7 @@ public class Config {
     private boolean collectSesame;
     private boolean zcjSignIn;
     private boolean merchantKmdk;
+    private boolean greenFinance;
 
     /* base */
     private static volatile Config config;
@@ -1116,8 +1122,21 @@ public class Config {
         hasChanged = true;
     }
 
+    public static List<String> stallInviteShopList() {
+        return getConfig().stallInviteShopList;
+    }
+
     public static boolean stallInviteRegister() {
         return getConfig().stallInviteRegister;
+    }
+
+    public static void setStallThrowManure(boolean b) {
+        getConfig().stallThrowManure = b;
+        hasChanged = true;
+    }
+
+    public static boolean stallThrowManure() {
+        return getConfig().stallThrowManure;
     }
 
     /* other */
@@ -1184,6 +1203,21 @@ public class Config {
         return getConfig().syncStepCount;
     }
 
+    private static int tmpStepCount = -1;
+    public static int tmpStepCount() {
+        if (tmpStepCount >= 0) {
+            return tmpStepCount;
+        }
+        tmpStepCount = Config.syncStepCount();
+        if (tmpStepCount > 0) {
+            tmpStepCount = RandomUtils.nextInt(tmpStepCount, tmpStepCount + 2000);
+            if (tmpStepCount > 100000) {
+                tmpStepCount = 100000;
+            }
+        }
+        return tmpStepCount;
+    }
+
     public static void setKbSginIn(boolean b) {
         getConfig().kbSignIn = b;
         hasChanged = true;
@@ -1245,6 +1279,15 @@ public class Config {
 
     public static boolean merchantKmdk() {
         return getConfig().merchantKmdk;
+    }
+
+    public static void setGreenFinance(boolean b) {
+        getConfig().greenFinance = b;
+        hasChanged = true;
+    }
+
+    public static boolean greenFinance() {
+        return getConfig().greenFinance;
     }
 
     /* base */
@@ -1385,6 +1428,8 @@ public class Config {
         c.stallSelfOpenTime = 120;
         c.stallDonate = false;
         c.stallInviteRegister = false;
+        c.stallThrowManure = false;
+        c.stallInviteShopList = new ArrayList<>();
 
         c.receivePoint = true;
         c.openTreasureBox = true;
@@ -1398,6 +1443,7 @@ public class Config {
         c.collectSesame = false;
         c.zcjSignIn = false;
         c.merchantKmdk = false;
+        c.greenFinance = false;
         return c;
     }
 
@@ -1834,6 +1880,16 @@ public class Config {
 
             config.stallInviteRegister = jo.optBoolean(jn_stallInviteRegister, true);
 
+            config.stallThrowManure = jo.optBoolean(jn_stallThrowManure, false);
+
+            config.stallInviteShopList = new ArrayList<>();
+            if (jo.has(jn_stallInviteShopList)) {
+                ja = jo.getJSONArray(jn_stallInviteShopList);
+                for (int i = 0; i < ja.length(); i++) {
+                    config.stallInviteShopList.add(ja.getString(i));
+                }
+            }
+
             /* other */
             config.receivePoint = jo.optBoolean(jn_receivePoint, true);
             Log.i(TAG, jn_receivePoint + ":" + config.receivePoint);
@@ -1876,11 +1932,14 @@ public class Config {
 
             config.merchantKmdk = jo.optBoolean(jn_merchantKmdk, true);
             Log.i(TAG, jn_merchantKmdk + ":" + config.merchantKmdk);
+
+            config.greenFinance = jo.optBoolean(jn_greenFinance, false);
+            Log.i(TAG, jn_greenFinance + ":" + config.greenFinance);
         } catch (Throwable t) {
             Log.printStackTrace(TAG, t);
             if (json != null) {
                 Log.i(TAG, "配置文件格式有误，已重置配置文件并备份原文件");
-                Log.infoChanged("配置文件格式有误，已重置配置文件并备份原文件", json);
+                Log.infoChanged(TAG, "配置文件格式有误，已重置配置文件并备份原文件");
                 FileUtils.write2File(json, FileUtils.getBackupFile(FileUtils.getConfigFile()));
             }
             config = defInit();
@@ -1888,8 +1947,7 @@ public class Config {
         String formatted = config2Json(config);
         if (!formatted.equals(json)) {
             Log.i(TAG, "重新格式化 config.json");
-            Log.infoChanged("重新格式化 config.json，原", json);
-            Log.infoChanged("重新格式化 config.json，新", formatted);
+            Log.infoChanged(TAG, "重新格式化 config.json");
             FileUtils.write2File(formatted, FileUtils.getConfigFile());
         }
         return config;
@@ -2162,6 +2220,12 @@ public class Config {
             jo.put(jn_stallSelfOpenTime, config.stallSelfOpenTime);
             jo.put(jn_stallDonate, config.stallDonate);
             jo.put(jn_stallInviteRegister, config.stallInviteRegister);
+            jo.put(jn_stallThrowManure, config.stallThrowManure);
+            ja = new JSONArray();
+            for (int i = 0; i < config.stallInviteShopList.size(); i++) {
+                ja.put(config.stallInviteShopList.get(i));
+            }
+            jo.put(jn_stallInviteShopList, ja);
 
             /* other */
             jo.put(jn_receivePoint, config.receivePoint);
@@ -2191,6 +2255,8 @@ public class Config {
             jo.put(jn_zcjSignIn, config.zcjSignIn);
 
             jo.put(jn_merchantKmdk, config.merchantKmdk);
+
+            jo.put(jn_greenFinance, config.greenFinance);
 
         } catch (Throwable t) {
             Log.printStackTrace(TAG, t);
